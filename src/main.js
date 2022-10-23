7cfd78f9325c89ffe7b70e84f57b66c8162adced
carlos-kennedy <carlosjustcarlos@outlook.com>
2022-10-23 15:19:38 -0300

    Update countdown

--- src/main.js
+++ src/main.js
@@ -11,21 +11,29 @@ let countDownDate = new Date("Oct 31, 2022 15:37:15").getTime();
 const countDownEvery1Second = setInterval(function () {
   const currentDate = new Date().getTime();
   const distanceNowAndCountDownDate = countDownDate - currentDate;
-  const d =
-    "0" +
-    Math.floor(distanceNowAndCountDownDate / (1000 * 60 * 60 * 24)) +
-    " : ";
-  const h =
-    Math.floor(
-      (distanceNowAndCountDownDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
-    ) + " : ";
-  const m =
-    Math.floor((distanceNowAndCountDownDate % (1000 * 60 * 60)) / (1000 * 60)) +
-    " : ";
+  const d = Math.floor(distanceNowAndCountDownDate / (1000 * 60 * 60 * 24));
+  const h = Math.floor(
+    (distanceNowAndCountDownDate % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+  );
+  const m = Math.floor(
+    (distanceNowAndCountDownDate % (1000 * 60 * 60)) / (1000 * 60)
+  );
   const s = Math.floor((distanceNowAndCountDownDate % (1000 * 60)) / 1000);
-
-  timer[0].innerHTML = d;
-  timer[1].innerHTML = h;
-  timer[2].innerHTML = m;
-  timer[3].innerHTML = s;
-}, 1000);
+  if (distanceNowAndCountDownDate <= 0) {
+    document.querySelector("ul").innerHTML = "O FOGUETE JÁ FOI LANÇADO";
+    clearInterval(x);
+    const d = date.getDay() + 1;
+    const h = date.getHours();
+    const m = date.getMinutes();
+    const s = date.getSeconds();
+    timer[0].innerHTML = d <= 9 ? "0" + d + " : " : d + " : ";
+    timer[1].innerHTML = h <= 9 ? "0" + h + " : " : h + " : ";
+    timer[2].innerHTML = m <= 9 ? "0" + m + " : " : m + " : ";
+    timer[3].innerHTML = s <= 9 ? "0" + s : s;
+  } else {
+    timer[0].innerHTML = d <= 9 ? "0" + d + " : " : d + " : ";
+    timer[1].innerHTML = h <= 9 ? "0" + h + " : " : h + " : ";
+    timer[2].innerHTML = m <= 9 ? "0" + m + " : " : m + " : ";
+    timer[3].innerHTML = s <= 9 ? "0" + s : s;
+  }
+});
